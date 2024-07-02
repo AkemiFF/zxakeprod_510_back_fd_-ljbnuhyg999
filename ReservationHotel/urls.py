3fc66e09bbaf96e55869f63a9e28eb0d06ad393b
Arsene Ratsimbason <ratsimbasonarsene1@gmail.com>
2024-07-02 06:30:00 +0200

    Ajout de fonctionnalité X sur la branche nouvelle-fonctionnalite

---
+++ ReservationHotel/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    
+    # Creation responsable etablissement
+    path('responsable/', include('ResponsableEtablissement.urls')),
+]
---
+++ ResponsableEtablissement/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.core.validators import RegexValidator
+
+# Responsable etablissement
+class Responsable_etablissement(models.Model):
+    email_responsable = models.EmailField()
+    nom_responsable = models.CharField(max_length=100)
+    prenom_responsable = models.CharField(max_length=100)
+    mdp_responsable = models.CharField(max_length=100)
+    numero_responsable = models.CharField(max_length=10, validators=[RegexValidator(regex=r'^(032|033|034|038)\d{7}$',message='Le numéro doit commencer par 032, 033, 034 ou 038 et contenir 7 chiffres supplémentaires.')])
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
