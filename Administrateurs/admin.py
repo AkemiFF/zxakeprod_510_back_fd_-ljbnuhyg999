29f06043bc6be788dc10054f6b002dbab961cb91
Brad Akemi <mirado.akemi@gmail.com>
2024-07-03 14:26:41 +0300

    Mise à Jour DB

--- Administrateurs/admin.py
+++ Administrateurs/admin.py
@@ -2,7 +2,3 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(TypeResponsable),
-admin.site.register(ResponsableEtablissement),
-admin.site.register(Hebergement),
-admin.site.register(HebergementImage),
-
