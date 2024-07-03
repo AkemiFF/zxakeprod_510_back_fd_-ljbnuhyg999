93e9923901f02bf73b9da6671d613ce2444aa5ff
Mirado Ake <mirado.akemi@gmail.com>
2024-07-03 16:43:46 +0300

    models

--- Accounts/models.py
+++ Accounts/models.py
@@ -21,7 +21,9 @@ class ResponsableEtablissement(models.Model):
     email_responsable = models.EmailField()
     nom_responsable = models.CharField(max_length=100)
     prenom_responsable = models.CharField(max_length=100)
+
     password_responsable = models.CharField(max_length=100)
+
     numero_responsable = models.CharField(max_length=10, validators=[RegexValidator(
         regex=r'^(032|033|034|038)\d{7}$', message='Le numéro doit commencer par 032, 033, 034 ou 038 et contenir 7 chiffres supplémentaires.')])
     created_at = models.DateTimeField(auto_now_add=True)
