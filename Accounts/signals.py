cbd61d018966d628a3be3498b6d00306075ac458
Mirado Ake <mirado.akemi@gmail.com>
2024-07-03 12:26:19 +0300

    modification signals

--- Accounts/signals.py
+++ Accounts/signals.py
@@ -1,14 +1,18 @@
 from django.db.models.signals import post_migrate
 from django.dispatch import receiver
-from Accounts.models import TypeCarteBancaire
+import Accounts
+from Accounts.models import TypeCarteBancaire, TypeResponsable
 
 
 @receiver(post_migrate)
-def create_initial_type_carte_bancaire(sender, **kwargs):
-    if sender.name == 'Accounts':
-        TypeCarteBancaire.objects.get_or_create(name="VISA")
-        TypeCarteBancaire.objects.get_or_create(name="MasterCard")
-        TypeCarteBancaire.objects.get_or_create(name="American Express")
-        TypeCarteBancaire.objects.get_or_create(name="Discover")
-        TypeCarteBancaire.objects.get_or_create(name="Diners Club")
-        TypeCarteBancaire.objects.get_or_create(name="JCB")
+def create_initial_types(sender, **kwargs):
+
+    TypeResponsable.objects.get_or_create(type_name="Hotel")
+    TypeResponsable.objects.get_or_create(type_name="Artisanal")
+    TypeResponsable.objects.get_or_create(type_name="Tour Operateur")
+
+    TypeCarteBancaire.objects.get_or_create(name="VISA")
+    TypeCarteBancaire.objects.get_or_create(name="MasterCard")
+    TypeCarteBancaire.objects.get_or_create(name="American Express")
+    TypeCarteBancaire.objects.get_or_create(name="Discover")
+    TypeCarteBancaire.objects.get_or_create(name="JCB")
