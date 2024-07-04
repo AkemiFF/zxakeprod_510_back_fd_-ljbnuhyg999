a8d131ac44a0fa9ae5db82d674a226541682edd4
Daniel <Razdaniel25@gmail.com>
2024-07-04 08:05:02 +0300

    Create views.py  pour Hebergements

--- Hebergement/views.py
+++ Hebergement/views.py
@@ -23,3 +23,33 @@ def get_count(request):
     return Response(serializer.data)
 
 
+@api_view(['POST'])
+def post_count(request, pk):
+    serializer = HebergementSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+def put_count(request, pk):
+    try:
+        type_responsable = Hebergement.objects.get(pk=pk)
+    except Hebergement.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    serializer = Hebergement(type_responsable, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE'])
+def delete_count(request, pk):
+    try:
+        type_responsable = Hebergement.objects.get(pk=pk)
+    except Hebergement.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    type_responsable.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
